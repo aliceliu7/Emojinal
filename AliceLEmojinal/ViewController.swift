4962d3790bee12a0c4279b66c46757d2286f1856
Apple <apple@Apples-MacBook-Pro-66.local>
2019-07-25 16:44:41 -0400

    EmojinalConfiguration1

--- AliceLEmojinal/ViewController.swift
+++ AliceLEmojinal/ViewController.swift
@@ -9,8 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let alertControl = UIAlertController(title: "Emojinal", message: "Info on Hearts", preferredStyle: UIAlertController.Style.alert)
+alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+present(alertController, animated: true, completion: nil)
+    
+    @IBOutlet weak var fieldTextRed: UIButton!
+    @IBOutlet weak var fieldTextBlack: UIButton!
+    @IBOutlet weak var fieldTextOrange: UIButton!
+    @IBAction func showMessage(sender: UIButton) {
 
+        
+        
+        
     override func viewDidLoad() {
+        }
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
